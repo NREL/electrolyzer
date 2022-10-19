3c65d592e4d53a06f597ccc6f756b4ab630b6681
bayc <christopher.j.bay@gmail.com>
2022-10-19 14:35:47 -0600

    remove CLI

--- tests/test_electrolyzer.py
+++ tests/test_electrolyzer.py
@@ -7,7 +7,6 @@ import pytest
 from click.testing import CliRunner
 
 from electrolyzer import electrolyzer
-from electrolyzer import cli
 
 
 @pytest.fixture
@@ -24,14 +23,3 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'electrolyzer.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
