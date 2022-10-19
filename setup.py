4378556bfdb2a7bf7a3c2d372c18840b91395b67
bayc <christopher.j.bay@gmail.com>
2022-10-19 14:36:41 -0600

    update setup.py file to follow similar NREL format

--- setup.py
+++ setup.py
@@ -2,7 +2,30 @@
 
 """The setup script."""
 
+from pathlib import Path
 from setuptools import setup, find_packages
+
+# Package meta-data.
+NAME = "Electrolyzer"
+DESCRIPTION = "A controls-oriented engineering electrolyzer model."
+URL = "https://github.com/NREL/electrolyzer"
+EMAIL = "christopher.bay@nrel.gov"
+AUTHOR = "NREL National Wind Technology Center"
+REQUIRES_PYTHON = ">=3.8.0"
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    "numpy",
+    "scipy",
+    "matplotlib",
+    "pandas",
+]
+
+# What packages are optional?
+EXTRAS = {
+    "docs": {"readthedocs-sphinx-ext", "Sphinx", "sphinxcontrib-napoleon"},
+    "develop": {"pytest>=3"},
+}
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -10,40 +33,38 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
-
 test_requirements = ['pytest>=3', ]
 
+ROOT = Path(__file__).parent
+with open(ROOT / "floris" / "version.py") as version_file:
+    VERSION = version_file.read().strip()
+
 setup(
-    author="Christopher Bay",
-    author_email='christopher.bay@nrel.gov',
-    python_requires='>=3.6',
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=readme + '\n\n' + history,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(include=['electrolyzer', 'electrolyzer.*']),
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license="Apache Software License 2.0",
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
-    description="Electrolyzer contains performance and cost modeling of H2 production.",
-    entry_points={
-        'console_scripts': [
-            'electrolyzer=electrolyzer.cli:main',
-        ],
-    },
-    install_requires=requirements,
-    license="Apache Software License 2.0",
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
-    keywords='electrolyzer',
-    name='electrolyzer',
-    packages=find_packages(include=['electrolyzer', 'electrolyzer.*']),
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/bayc/electrolyzer',
-    version='0.1.0',
     zip_safe=False,
 )
