9d0628d365e094c4324b02ec6c6eb8d50bb00fb4
Cameron Irmas <cirmas@protonmail.com>
2022-10-22 13:06:10 -0700

    [ref #4] Add CI linting workflow using black

--- electrolyzer/__init__.py
+++ electrolyzer/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Electrolyzer."""
 
 __author__ = """Christopher Bay"""
-__email__ = 'christopher.bay@nrel.gov'
-__version__ = '0.1.0'
+__email__ = "christopher.bay@nrel.gov"
+__version__ = "0.1.0"
--- electrolyzer/electrolyzer.py
+++ electrolyzer/electrolyzer.py
@@ -1,1 +1,3 @@
 """Main module."""
+
+bad = [1]  # this should fail linter
