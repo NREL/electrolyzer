9f24ff5f7f857b74b7282afec87e64a8f50dd608
Cameron Irmas <cirmas@protonmail.com>
2022-10-22 12:06:23 -0700

    [ref #4] Add black, isort, flake8 pre-commit

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@
 """The setup script."""
 
 from pathlib import Path
+
 from setuptools import setup, find_packages
+
 
 # Package meta-data.
 NAME = "Electrolyzer"
@@ -24,47 +26,49 @@ REQUIRED = [
 # What packages are optional?
 EXTRAS = {
     "docs": {"readthedocs-sphinx-ext", "Sphinx", "sphinxcontrib-napoleon"},
-    "develop": {"pytest>=3"},
+    "develop": {"pytest>=3", "pre-commit"},
 }
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 ROOT = Path(__file__).parent
-with open(ROOT / "electrolyzer"/ "version") as version_file:
+with open(ROOT / "electrolyzer" / "version") as version_file:
     VERSION = version_file.read().strip()
 
 setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(include=['electrolyzer', 'electrolyzer.*']),
+    packages=find_packages(include=["electrolyzer", "electrolyzer.*"]),
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
     license="Apache Software License 2.0",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
     zip_safe=False,
 )
