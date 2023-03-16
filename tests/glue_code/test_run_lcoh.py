15e0682d620dfb3c664a4397dc4e2325aa8c1885
bayc <christopher.j.bay@gmail.com>
2023-03-16 15:36:28 -0600

    update format for test_run_lcoh.py

--- tests/glue_code/test_run_lcoh.py
+++ tests/glue_code/test_run_lcoh.py
@@ -9,16 +9,13 @@ from electrolyzer import run_lcoh
 
 lcoh_breakdown = pd.DataFrame(
     {
-        'Life Totals [$]': [5.388657e+06, 1.079412e+06, 1.197895e+07, 1.283473e+06],
-        'Life Totals [$/kg-H2]': [1.359484, 0.272321, 3.022124, 0.323803]
+        "Life Totals [$]": [5.388657e06, 1.079412e06, 1.197895e07, 1.283473e06],
+        "Life Totals [$/kg-H2]": [1.359484, 0.272321, 3.022124, 0.323803],
     },
-    index=['CapEx','OM','Feedstock','Stack Rep']
+    index=["CapEx", "OM", "Feedstock", "Stack Rep"],
 )
 
-RESULT = (
-    lcoh_breakdown,
-    4.9777312843759915
-)
+RESULT = (lcoh_breakdown, 4.9777312843759915)
 ROOT = Path(__file__).parent.parent.parent
 
 
@@ -26,7 +23,7 @@ def test_run_lcoh():
     fname_input_modeling = os.path.join(
         ROOT, "examples", "example_04_lcoh", "cost_modeling_options.yaml"
     )
-    
+
     turbine_rating = 3.4  # MW
 
     # Create cosine test signal
@@ -39,5 +36,11 @@ def test_run_lcoh():
 
     calc_result = run_lcoh(fname_input_modeling, power_test_signal, lcoe)
 
-    assert_frame_equal(calc_result[0]["LCOH Breakdown"], RESULT[0], check_dtype=False, check_exact=False, atol=1e-4)
+    assert_frame_equal(
+        calc_result[0]["LCOH Breakdown"],
+        RESULT[0],
+        check_dtype=False,
+        check_exact=False,
+        atol=1e-4,
+    )
     assert np.isclose(calc_result[1], RESULT[1])
