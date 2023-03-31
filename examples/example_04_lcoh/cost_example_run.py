e0173f7526b46c3fa2dbad213d98a82825cab794
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2023-03-31 15:00:24 -0600

    Cost Feature Added for LCOH (#26)
    * Cost Feature Added
    
    initial LCOH estimation and calculation based on literature, user can input annual data or an instance of the electrolyzer supervisor to get a simple annual estimation. There are two different OpEx calculations that have different base cost values for the stack replacement costs
    
    * Added pre-commit tests to commit
    
    * LCOH integrated with new model file and hopefully works with repo
    
    * update lcoh example and add test for run_lcoh
    
    * remove old files
    
    * update format for test_run_lcoh.py
    
    ---------
    
    Co-authored-by: bayc <christopher.j.bay@gmail.com>

---
+++ examples/example_04_lcoh/cost_example_run.py
@@ -0,0 +1,25 @@
+"""
+This example performs an LCOH calculation for an electrolyzer system.
+"""
+import os
+import numpy as np
+
+from electrolyzer import run_lcoh
+
+
+fname_input_modeling = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "cost_modeling_options.yaml"
+)
+
+turbine_rating = 3.4  # MW
+
+# Create cosine test signal
+test_signal_angle = np.linspace(0, 8 * np.pi, 3600 * 8 + 10)
+base_value = (turbine_rating / 2) + 0.2
+variation_value = turbine_rating - base_value
+power_test_signal = (base_value + variation_value * np.cos(test_signal_angle)) * 1e6
+
+lcoe = 44.18 * (1 / 1000)
+res = run_lcoh(fname_input_modeling, power_test_signal, lcoe)
+
+print(res)
