f2135ca4c6e1aa77c2a6fea9c70acd864b43b078
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-07-13 14:00:56 -0600

    starting playing with example 00 to test functionality

--- examples/example_00_refactor/run.py
+++ examples/example_00_refactor/run.py
@@ -8,3 +8,16 @@ os.chdir(Path(__file__).parent)
 config_fpath = Path(__file__).parent / "bert_config.yaml"
 bert = BERT(config_fpath)
 bert.run()
+
+scale_fac = bert.model.get_val("Cluster0.n_stacks", units="unitless") * bert.model.get_val(
+    "Cluster0.n_cells", units="unitless"
+)
+
+p_cell_ref = bert.model.get_val("Cluster0.converter.ref_cell.P_cell_out", units="W")
+p_system_ref = p_cell_ref * scale_fac
+bert.model.set_val("controller.P_command", p_cell_ref, units="W")
+bert.setup()
+bert.run()
+i_out = bert.model.get_val("Cluster0.converter.p2i.I_command", units="A")
+I_input = bert.model.get_val("Cluster0.converter.I_ref_points", units="A")
+i_error = i_out - I_input
