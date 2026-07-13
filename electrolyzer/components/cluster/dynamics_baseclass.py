d944e8c44047a2f4ced50865672ac2a3df67620e
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-07-13 10:48:02 -0600

    translated dynamics, degradation, and PI curve building blocks into baseclasses

---
+++ electrolyzer/components/cluster/dynamics_baseclass.py
@@ -0,0 +1,23 @@
+import openmdao.api as om
+
+
+class DynamicsBase(om.ExplicitComponent):
+    def initialize(self):
+        self.options.declare("tech_config", types=dict, default={})
+
+    def setup(self):
+        self.add_input("I_in", val=0.0, shape_by_conn=True, units="A")
+        self.add_input("I_min", val=0.0, shape=1, units="A")
+        self.add_input("I_max", val=0.0, shape=1, units="A")
+
+        self.add_output("I_out", val=0.0, copy_shape="I_in", units="A")
+        self.add_output("on_off_status", val=0.0, copy_shape="I_in", units="unitless")
+
+    def compute(self, inputs, outputs, discrete_inputs, discrete_outputs):
+        """
+        Computation for the OM component.
+
+        For a template class this is not implement and raises an error.
+        """
+
+        raise NotImplementedError("This method should be implemented in a subclass.")
---
+++ electrolyzer/translators/power_translator_baseclass.py
@@ -0,0 +1,24 @@
+import openmdao.api as om
+
+
+class PowerToCurrentBase(om.ExplicitComponent):
+    def initialize(self):
+        self.options.declare("tech_config", types=dict, default={})
+
+    def setup(self):
+        self.add_input("I_ref_points", val=0.0, shape_by_conn=True, units="A")
+        # self.add_input("J_ref_points", val=0.0, copy_shape="I_ref_points", units="A/(cm**2)")
+        self.add_input("P_ref_points", val=0.0, copy_shape="I_ref_points", units="W")
+        # NOTE: could use V_ref_points instead and calculate power in compute()
+
+        self.add_input("P_command", val=0.0, shape_by_conn=True, units="W")
+        self.add_output("I_command", val=0.0, copy_shape="P_command", units="A")
+
+    def compute(self, inputs, outputs, discrete_inputs, discrete_outputs):
+        """
+        Computation for the OM component.
+
+        For a template class this is not implement and raises an error.
+        """
+
+        raise NotImplementedError("This method should be implemented in a subclass.")
