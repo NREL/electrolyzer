491416b9994aed726bd6a57522ed57d435be1644
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-07-13 11:11:55 -0600

    updated cluster.py to use dynamics baseclass

--- electrolyzer/components/cluster/cluster.py
+++ electrolyzer/components/cluster/cluster.py
@@ -1,17 +1,16 @@
 import numpy as np
-import openmdao.api as om
 from attrs import field, define, validators
 
 from electrolyzer.core.utilities import BaseConfig
-from electrolyzer.tools.validators import range_val
+from electrolyzer.components.cluster.dynamics_baseclass import DynamicsBase
 
 
 @define(kw_only=True)
-class ClusterBaseConfig(BaseConfig):
-    n_stacks: int = field(converter=int, validator=validators.gt(0.0))
+class SimpleDynamicsConfig(BaseConfig):
+    # n_stacks: int = field(converter=int, validator=validators.gt(0.0))
     # j_max: float = field(validator=validators.ge(0.0))
-    i_max: float = field(validator=validators.ge(0.0))
-    turndown_ratio: float = field(validator=range_val(0.0, 1.0))
+    # i_max: float = field(validator=validators.ge(0.0))
+    # turndown_ratio: float = field(validator=range_val(0.0, 1.0))
     include_startup_delay: bool = field(default=False)
     off_hours_cold_start: float = field(
         default=None, validator=validators.optional(validators.gt(0.0))
@@ -21,7 +20,7 @@ class ClusterBaseConfig(BaseConfig):
     )
 
 
-class ClusterBaseClass(om.ExplicitComponent):
+class SimpleDynamics(DynamicsBase):
     def initialize(self):
         self.options.declare("plant_config", types=dict)
         self.options.declare("tech_config", types=dict)
@@ -29,21 +28,17 @@ class ClusterBaseClass(om.ExplicitComponent):
     def setup(self):
         # self.n_timesteps = self.options["plant_config"]["simulation"]["n_timesteps"]
         self.dt = self.options["plant_config"]["simulation"]["dt"]
-        self.config = ClusterBaseConfig.from_dict(self.options["tech_config"]["cluster_parameters"])
+        self.config = SimpleDynamicsConfig.from_dict(
+            self.options["tech_config"]["cluster_parameters"]
+        )
 
+        super().setup()
         # design variables
-        self.add_input("n_stacks", val=self.config.n_stacks, units="unitless")
-
-        self.add_input("current_cmd", val=0.0, shape_by_conn=True, units="A")
-
-        self.add_output("on_off_status", val=0.0, copy_shape="current_cmd", units="unitless")
-        self.add_output("current_out", val=0.0, copy_shape="current_cmd", units="A")
 
     def compute(self, inputs, outputs):
-        # NOTE: need cell area as an input to use j_max as a bound
-        i_min = self.config.turndown_ratio * self.config.i_max
-        on_off_status = np.where(inputs["current_cmd"] < i_min, 0, 1)
+        on_off_status = np.where(inputs["I_in"] < inputs["I_min"], 0, 1)
         # i_out should also reflect warm-up losses or delays
-        i_out = np.clip(inputs["current_cmd"], a_min=None, a_max=self.config.i_max)
-        outputs["current_out"] = i_out
+        i_out = np.clip(inputs["I_in"], a_min=inputs["I_min"], a_max=inputs["I_max"])
+        # TODO: add start-up delay
+        outputs["I_out"] = i_out
         outputs["on_off_status"] = on_off_status
